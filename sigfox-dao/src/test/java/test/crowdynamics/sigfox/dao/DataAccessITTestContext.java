0bc37c465758e198a35f50707449bcb5c6f632a0
Luis <luissaizalvarez@gmail.com>
2018-08-29 18:16:02 +0200

    cambio estructura

--- sigfox-dao/src/test/java/test/crowdynamics/sigfox/dao/DataAccessITTestContext.java
+++
@@ -1,8 +0,0 @@
-package test.crowdynamics.sigfox.dao;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.PropertySource;
-
-@ComponentScan("com.crowdynamics.sigfox.dao")
-public class DataAccessITTestContext {
-}
