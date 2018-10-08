86dff933e22d1188721500de94a899e1871a3207
mrbq <mrivasbq@icloud.com>
2018-10-08 21:35:03 +0200

    Optimizar imports

--- src/main/java/com/crowdynamics/sigfox/controllers/SigfoxController.java
+++ src/main/java/com/crowdynamics/sigfox/controllers/SigfoxController.java
@@ -1,23 +1,16 @@
 package com.crowdynamics.sigfox.controllers;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.crowdynamics.sigfox.converters.SigfoxMessageDtoToMessageConverter;
 import com.crowdynamics.sigfox.converters.SigfoxMessageToDtoConverter;
 import com.crowdynamics.sigfox.dto.SigfoxMessageDto;
 import com.crowdynamics.sigfox.model.SigfoxMessage;
 import com.crowdynamics.sigfox.services.SigFoxMessageServiceImpl;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/sigfox")
